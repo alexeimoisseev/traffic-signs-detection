283951337f0db62397459ee373bb6329cd8ae587
Alexei Moisseev <alexmois@ya.ru>
2021-05-08 14:42:34 +0300

    ignore ssl errors (:shrug:)

--- get_data.sh
+++ get_data.sh
@@ -3,8 +3,8 @@
 mkdir -v -p data/SwedishSigns
 
 if [ ! -f "data/SwedishSigns/Set1Part0.zip" ]; then
-  curl -o data/SwedishSigns/annotations.txt 'https://www.cvl.isy.liu.se/research/trafficSigns/swedishSignsSummer/Set1/annotations.txt'
-  curl -o data/SwedishSigns/Set1Part0.zip.part 'https://www.cvl.isy.liu.se/research/trafficSigns/swedishSignsSummer/Set1/Set1Part0.zip'
+  curl -k -o data/SwedishSigns/annotations.txt 'https://www.cvl.isy.liu.se/research/trafficSigns/swedishSignsSummer/Set1/annotations.txt'
+  curl -k -o data/SwedishSigns/Set1Part0.zip.part 'https://www.cvl.isy.liu.se/research/trafficSigns/swedishSignsSummer/Set1/Set1Part0.zip'
   mv -v data/SwedishSigns/Set1Part0.zip{.part,}
 fi
 

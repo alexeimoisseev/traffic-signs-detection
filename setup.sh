58bd92f77d8e04201c2764960795841942381e80
Alexei Moisseev <alexmois@ya.ru>
2021-05-20 11:18:43 +0300

    auto config for aws linux

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 git submodule update --init
-sudo yum install gcc python3-devel mesa-libGL
-sudo pip3 install --upgrade pip
+sudo yum install -y gcc python3-devel mesa-libGL xz-devel libffi-devel
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init --path)"' >> ~/.profile
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+pyenv install 3.7.10
 pip install -r yolov5/requirements.txt
 ./get_data.sh
